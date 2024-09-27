8bc13dbf706bd7375a61062912fd905768676a14
dev-088 <dev088.dg@gmail.com>
2024-09-27 23:27:01 +0530

    C

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const temp =document.getElementById( 'Prevention');
+
+
+temp.addEventListener('click', () => {
+
+  //alert("buttokn is clicke");
+
+container.innerHTML= ` 
+  
+  <select>
+  <option> DERMO </option>
+  </select>
+  
+  `;
+
+})
+
+
+
+  
+ 
+ 
+
+
+
